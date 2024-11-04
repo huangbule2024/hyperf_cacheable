f4e1d06beb41c75cabaef2013d3ab8df123bf9e3
wuque <91714264+huangbule2024@users.noreply.github.com>
2024-11-04 09:10:54 +0800

    Update CacheableTrait.php

--- src/Traits/CacheableTrait.php
+++ src/Traits/CacheableTrait.php
@@ -7,7 +7,7 @@ namespace App\Traits;
 use App\Kernel\Cache\CacheQueryBuilder;
 
 /**
- * 缓存，适合数据表数据不是太大，大表慎用,因为增删改会触发全部这个表相关联缓存删除
+ * 缓存，适合数据表数据不是太大，大表慎用
  * @author hbl
  * @date 2024/09/13
  */
@@ -20,14 +20,13 @@ trait CacheableTrait
      */
     protected function newBaseQueryBuilder()
     {
-        $conn = $this->getConnection();
-
+        $connection = $this->getConnection();
         return new CacheQueryBuilder(
-            $conn,
-            $conn->getConnection(),
-            $conn->getPostProcessor()
+            $connection,
+            $connection->getQueryGrammar(),
+            $connection->getPostProcessor(),
+            $this
         );
     }
-
 
 }
